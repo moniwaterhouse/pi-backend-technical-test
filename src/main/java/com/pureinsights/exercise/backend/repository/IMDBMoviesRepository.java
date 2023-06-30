c1ae3399f451a98ad89f39371ca6602b66c1e234
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2023-06-30 10:55:12 -0600

    Create rest api client

---
+++ src/main/java/com/pureinsights/exercise/backend/repository/IMDBMoviesRepository.java
@@ -0,0 +1,22 @@
+package com.pureinsights.exercise.backend.repository;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+
+public class IMDBMoviesRepository {
+
+    // Create the low-level client
+    RestClient restClient = RestClient.builder(
+            new HttpHost("localhost", 9200)).build();
+
+    // Create the transport with a Jackson mapper
+    ElasticsearchTransport transport = new RestClientTransport(
+            restClient, new JacksonJsonpMapper());
+
+    // And create the API client
+    ElasticsearchClient client = new ElasticsearchClient(transport);
+}
