7ffe002253ccc621a73331848049ffc928e02b6a
Moni Waterhouse <monica.waterhouse@estudiantec.cr>
2023-07-03 15:23:15 -0600

    Create endpoint to get a list of movies which genre includes "action"

--- src/main/java/com/pureinsights/exercise/backend/repository/IMDBMoviesRepository.java
+++ src/main/java/com/pureinsights/exercise/backend/repository/IMDBMoviesRepository.java
@@ -1,22 +1,64 @@
 package com.pureinsights.exercise.backend.repository;
-
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.TotalHits;
+import co.elastic.clients.elasticsearch.core.search.TotalHitsRelation;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.pureinsights.exercise.backend.model.Movie;
+import com.pureinsights.exercise.backend.model.Product;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 
+import java.io.IOException;
+import java.util.List;
+
 public class IMDBMoviesRepository {
 
-    // Create the low-level client
-    RestClient restClient = RestClient.builder(
-            new HttpHost("localhost", 9200)).build();
+    public static void main(String[] args) throws IOException {
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("localhost", 9200)).build();
 
-    // Create the transport with a Jackson mapper
-    ElasticsearchTransport transport = new RestClientTransport(
-            restClient, new JacksonJsonpMapper());
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
 
-    // And create the API client
-    ElasticsearchClient client = new ElasticsearchClient(transport);
+        // And create the API client
+        ElasticsearchClient client = new ElasticsearchClient(transport);
+
+        SearchResponse<Movie> actionMovies = client.search(s -> s
+                        .index("imdb-movies")
+                        .query(q -> q
+                                .match(t -> t
+                                        .field("genre")
+                                        .query("Action")
+                                )
+                        ),
+                Movie.class
+        );
+
+        TotalHits totalActionMovies = actionMovies.hits().total();
+
+        System.out.println("Number of action movies: " + totalActionMovies.value());
+
+
+        List<Hit<Movie>> actionMoviesList = actionMovies.hits().hits();
+
+        int count = 1;
+
+        System.out.println("List of action movies:");
+        for(Hit<Movie> actionMovie: actionMoviesList){
+            Movie movie = actionMovie.source();
+            System.out.println(count + ". " + movie.name);
+            count++;
+        }
+
+        restClient.close();
+
+    }
 }
